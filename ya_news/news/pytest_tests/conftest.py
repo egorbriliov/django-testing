922fcb4e503fd7b6e53ebc0aa8180a3fe4b5996b
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-04-19 18:49:51 +0300

    Создал фикстуры для автора и читателя, новости и коментария к ней, а так же ключа к ним.

---
+++ ya_news/news/pytest_tests/conftest.py
@@ -0,0 +1,57 @@
+import pytest
+
+from django.test.client import Client
+
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def reader(django_user_model):
+    return django_user_model.objects.create(username='Читатель')
+
+
+@pytest.fixture
+def reader_client(reader, client):
+    client.force_login(reader)
+    return client
+
+
+@pytest.fixture
+def news():
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст заметки',
+    )
+    return news
+
+
+@pytest.fixture
+def news_pk(news):
+    return (news.pk,)
+
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='text'
+    )
+    return comment
+
+
+@pytest.fixture
+def comment_pk(comment):
+    return (comment.pk, )
